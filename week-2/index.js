1ed6123116639e50f25f620558a91d2880ea2c4e
dev-93 <cr000@naver.com>
2020-11-03 01:43:39 +0900

    문자열 내 마음대로 정렬하기 수정

--- week-2/index.js
+++ week-2/index.js
@@ -1,24 +1,24 @@
 // 문자열 내 마음대로 정렬하기
 
-// const strings = ["sun", "bed", "car"];
-// let n = 1;
+const strings = ["sun", "bed", "car"];
+let n = 1;
 
 // const strings = ["abce", "abcd", "cdx"];
 // const n = 2;
 
-// function solution(strings, n) {
-//     return strings.sort((a, b) => {
-//         const chr1 = a.charAt(n);
-//         const chr2 = b.charAt(n);
+function solution(strings, n) {
+    return strings.sort((a, b) => {
+        const chr1 = a.charAt(n);
+        const chr2 = b.charAt(n);
+        if (chr1 === chr2) {
+            return (a > b) - (a < b);
+        } else {
+            return (chr1 > chr2) - (chr1 < chr2);
+        }
+    });
+}
 
-//         if (chr1 == chr2) {
-//             return (a > b) - (a < b);
-//         } else {
-//             return (chr1 > chr2) - (chr1 < chr2);
-//         }
-//     });
-// }
-
+solution(strings, n);
 // console.log(solution(strings, n));
 
 // 2. 크레인 인형뽑기 게임
@@ -74,19 +74,19 @@
 // 최대 몇 개의 부서에 물품을 지원할 수 있는지 return 하도록 solution 함수를 완성해주세요.
 
 // key-point: 최대한 많은 부서의 물품을 구매해 줄 수 있도록
-const d = [1, 3, 2, 5, 4];
-const budget = 9;
+// const d = [1, 3, 2, 5, 4];
+// const budget = 9;
 
 // const d = [2,2,3,3];
 // const budget = 10;
 
-function solution(d, budget) {
-    d.sort((a, b) => a - b);
-    while (d.reduce((acc, cur) => (acc + cur), 0) > budget) d.pop();
-    return d.length;
-}
+// function solution(d, budget) {
+//     d.sort((a, b) => a - b);
+//     while (d.reduce((acc, cur) => (acc + cur), 0) > budget) d.pop();
+//     return d.length;
+// }
 
-console.log(solution(d, budget));
+// console.log(solution(d, budget));
 // solution(d, budget);
 
 
