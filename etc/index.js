34dbc73f09239a6f7d674be70fb04cc2fa7eebc2
dev-93 <cr000@naver.com>
2020-11-04 01:58:04 +0900

    두개 뽑아서 더하기 완성

--- etc/index.js
+++ etc/index.js
@@ -1,16 +1,22 @@
 // 두개 뽑아서 더하기
 
 function solution(numbers) {
-    var answer = [];
-    
-    for(var i=0; i < numbers.length; i++) {
-        // console.log(numbers[i]);
-        console.log(numbers[i+1]);
-        // numbers[i] + numbers[i+1];
+    var answer=[];
+    for(var i=0;i<numbers.length; i++) {
+        for(var j=0; j<numbers.length; j++) {
+            if(i === j) continue;
+            var sum = numbers[i] + numbers[j];
+            answer.push(sum);
+        }
     }
-    return answer;
+    return answer
+    .sort((a,b) => a-b)
+    .reduce(function(acc,cur){
+        if (acc.slice(-1)[0] !== cur) acc.push(cur);
+        return acc;
+    },[]);
 }
 
 // solution([2,1,3,4,1]);
-console.log(solution([2,1,3,4,1]));
-// solution([5,0,2,7]);
+// console.log(solution([2,1,3,4,1]));
+console.log(solution([5,0,2,7]));
