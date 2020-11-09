12204aa6318419471b70c4051bdaa4ecadebcb2c
dev-93 <cr000@naver.com>
2020-11-10 02:44:02 +0900

    best album 수정

--- week-3/bestAlbum.js
+++ week-3/bestAlbum.js
@@ -1,34 +1,44 @@
 // 베스트 앨범
 
+// 1. 총 플레이 횟수 많은 순으로 장르 별로 내림차순으로 정렬
+// 2. 장르 내에서도 플레이 횟수 내림차순, 고유 번호 오름차순으로 정렬
+// 한 장르 당 최대 2개까지만 뽑아서, 플레이 목록을 만들 수 있다.
+// 플레이 목록은 고유 번호로 하는 리스트이다.
 
 function solution(genres, plays) {
-    var list = genres.reduce((hash, g, i) => {
-        if (!hash[g]) {
-            hash[g] = {plays: plays[i], music: []};
-        } else {
-            hash[g].plays += plays[i];
-        }
-            hash[g].music.push([i, plays[i]]);
-        return hash;
-    }, {});
-    var answer = [];
-    var sorted = Object.values(list).sort((l, r) => r.plays - l.plays);
-    sorted.forEach((g) => {
-        if (g.music.length > 1) {
-            g.music.sort((l, r) => {
-                if (l[1] > r[1]) {
-                    return -1;
-                } else if (l[1] < r[1]) {
-                    return 1;
-                } else {``
-                    return (l[0] > r[0]) ? 1 : -1;
-                }
+    const songs = genres.map((genre, index) => {
+        return {
+            no: index,
+            genre: genre,
+            playCount: plays[index]    
+        };
+    });
+
+    const genrePlayCount = [];
+    
+    songs.forEach(song => {
+        const thisGenre = genrePlayCount.find(genrePlay => genrePlay.genre === song.genre);
+        if (!thisGenre) {
+            genrePlayCount.push({
+                genre: song.genre, totalPlayCount: song.playCount
             });
-            answer.push(g.music[0][0]);
-            answer.push(g.music[1][0]);
         } else {
-            answer.push(g.music[0][0]);
+            thisGenre.totalPlayCount += song.playCount;
         }
-        });
+    });
+
+    genrePlayCount.sort((a, b) => b.totalPlayCount - a.totalPlayCount);
+
+    const answer = [];
+    
+    genrePlayCount.forEach(genrePlay => {
+        const thisGenreSongs = songs.filter(song => genrePlay.genre === song.genre);
+        thisGenreSongs.sort((a, b) => b.playCount - a.playCount);
+        answer.push(thisGenreSongs[0].no);
+        if (thisGenreSongs.length > 1) {
+            answer.push(thisGenreSongs[1].no);
+        }
+    });
+
     return answer;
 }
