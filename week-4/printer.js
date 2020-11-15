7965952e5caacab8bc6b4d6ce94b4554b93a48b9
dev-93 <cr000@naver.com>
2020-11-15 22:33:40 +0900

    프린터

--- week-4/printer.js
+++ week-4/printer.js
@@ -1,25 +1,30 @@
-// 프린터 
-
-(function solution(priorities, loc) {
-    const answer = 0;
-
-    const docList = [];
+function solution(priorities, location) {
+    let answer = 0;
+    // priorities 배열의 인덱스를 요소로 갖는 prints 배열 선언
+    let prints = priorities.map((_, i) => i);
     
-    // docList에 내용을 추가한다.
-    priorities.map((list,index) => {
-        docList.push({
-            id:index,
-            prioritie: list,
-        })
-    });
-
-    // docList에 있는 prioritie의 내용을 내림차순으로 정렬한다.
-    docList.map((list) => console.log(list.prioritie));
-
-    return answer;
-})(priorities=[2,1,3,2], loc=2);  //return 1
-// })(priorities=[1,1,9,1,1,1], loc=0); // return 5
-
-
-
-
+    // prints 배열에 요소가 있을 동안 탐색
+    while(prints.length > 0){
+    	// tmp: prints의 맨 앞 요소(=priorities의 인덱스)
+        let tmp = prints.shift();
+        // priorities 배열 요소 중 priorities[tmp]보다 큰 우선순위가 있다면
+        for(let i = 0; i < priorities.length; i++){
+            if(priorities[i] > priorities[tmp]){
+            	// prints 배열 앞에서 빼냈던 tmp를 push로 뒤에 넣는다
+                prints.push(tmp);
+                break;
+            }
+        }
+        // 만약 prints 배열에서 tmp가 삭제됐다면
+        if(!prints.includes(tmp)){
+            // priorities 배열의 tmp 번째 인덱스를 -1로 채워넣고(작은 숫자가 우선순위 낮음)
+            priorities.fill(-1, tmp, tmp + 1);
+            // 삭제된 요소가 있기 때문에 answer에 +1 해준다
+            answer++;
+            // tmp가 location와 동일할 경우 answer 반환
+            if(tmp === location){
+                return answer;   
+            }
+        }
+    }
+}
