875115dd84cdb86c23cffe77ab9ab98d3973b429
dev-93 <cr000@naver.com>
2020-11-21 16:35:53 +0900

    코드 정리

--- week-5/kthNumber.js
+++ week-5/kthNumber.js
@@ -5,9 +5,8 @@ const commands = [[2,5,3],[4,4,1],[1,7,3]];
 // 2번째부터 5번째를 먼저 오름차순으로 정렬 한뒤 그 중에서 3번째 뽑기
 
 function solution(array, commands) {
-    let answer = commands.map((list) => 
+    const answer = commands.map((list) =>
         array.slice(list[0]-1, list[1]).sort((a,b) => a-b)[list[2] -1]);
-        console.log(answer);
     return answer;
 };
 
