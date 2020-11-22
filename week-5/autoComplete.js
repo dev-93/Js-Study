d5c81bb58cee9ef3adc436a60e672b0916a58a1a
dev-93 <cr000@naver.com>
2020-11-22 18:49:50 +0900

    test 1만 만족

--- week-5/autoComplete.js
+++ week-5/autoComplete.js
@@ -3,16 +3,27 @@ let words = ["go","gone","guild"];
 // let words = ['word','war','warrior','world'];
 
 function solution(words) {
-    var answer = 0;
+    let answer = 0;
+    words.sort();
+    // 학습된 단어들이 go, guild,gone으로 주어질 경우 대비해서 sort 했음. 
     words.map((word) => {
         console.log(word, word.length);
-        console.log(word.slice(0,2));
-        // if (word.length === 2) {
-        //     answer = answer + 2;
-        // } else if (word.length === 4 ){
-        //      answer = answer + (word.length -1)
-        // }
+        let firstTwoWords = word.slice(0,2);  
+        console.log(firstTwoWords);
+        let isExist = words.indexOf(firstTwoWords);
+        console.log(isExist);
+        if (isExist === 0) {
+            if (word.length === firstTwoWords.length) {
+                answer += word.length;
+            } else if (word.length !== firstTwoWords.length) {
+                answer += word.length -1
+            }
+        } else if(isExist === -1) {
+            answer +=2;
+        }
     })
+
+    // 예외처리는 words에 go gone gonz guild 가 있을때, gone과 gonz를 구분하려면 4글자를 쳐야함
     console.log(answer);
     return answer;
 };
