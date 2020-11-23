c5eeda048c8834d24d1dde856d8f7675d30de61c
dev-93 <cr000@naver.com>
2020-11-24 00:38:56 +0900

    자동완성 못품

--- week-5/autoComplete.js
+++ week-5/autoComplete.js
@@ -1,30 +1,20 @@
-let words = ["go","gone","guild"];
-// let words = ['abc','def','ghi','jklm'];
-// let words = ['word','war','warrior','world'];
-
 function solution(words) {
-    let answer = 0;
-    words.sort();
-    // 학습된 단어들이 go, guild,gone으로 주어질 경우 대비해서 sort 했음. 
-    words.map((word) => {
-        console.log(word, word.length);
-        let firstTwoWords = word.slice(0,2);  
-        console.log(firstTwoWords);
-        let isExist = words.indexOf(firstTwoWords);
-        console.log(isExist);
-        if (isExist === 0) {
-            if (word.length === firstTwoWords.length) {
-                answer += word.length;
-            } else if (word.length !== firstTwoWords.length) {
-                answer += word.length -1
-            }
-        } else if(isExist === -1) {
-            answer +=2;
+    words.sort((a, b) => a < b ? -1 : a > b ? 1 : 0);
+
+    let r = 0, a, b, c;
+
+    words.forEach((w, i) => {
+        const right = words[i + 1] || words[i - 1];
+        const left = words[i - 1] || words[i + 1];
+
+        for (let i = 1, l = w.length; i <= l; i++) {
+            r++;
+            a = w.substring(0, i);
+            if (a != left.substring(0, i) && a != right.substring(0, i))
+                return;
         }
-    })
+    });
 
-    // 예외처리는 words에 go gone gonz guild 가 있을때, gone과 gonz를 구분하려면 4글자를 쳐야함
-    return answer;
-};
+    return r;
 
-solution(words);
+}
