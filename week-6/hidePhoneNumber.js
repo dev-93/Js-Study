78107d71c8d6e2fd3a9e051de325b7c4fdd2914a
dev-93 <cr000@naver.com>
2020-11-30 01:12:41 +0900

    slice와 substr

--- week-6/hidePhoneNumber.js
+++ week-6/hidePhoneNumber.js
@@ -1,8 +1,12 @@
-// const phone_number = "01033334444";
+const phone_number = "01033334444";
 // const phone_number = "027778888";
 
 function solution(phone_number) {
-    return '*'.repeat(phone_number.length-4) + phone_number.slice(-4)
+    var temp1 = '*'.repeat(phone_number.length-4) + phone_number.slice(-4);
+    var temp2 = '*'.repeat(phone_number.length-4) + phone_number.substr(-4,4)
+    console.log(temp1);
+    console.log(temp2);
+    return temp1;
 };
 
 solution(phone_number);
